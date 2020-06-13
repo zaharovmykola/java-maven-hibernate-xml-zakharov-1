3b232b65d627c7a87b61855afc0815bd12a76088
zaharovmykola <zaharovmykola@gmail.com>
2020-06-13 07:32:53 +0200

    without-database

---
+++ src/main/org.mykola.zakharov.startwith.hibernate/java/DreamJob.java
@@ -0,0 +1,59 @@
+public class DreamJob {
+    private String name;
+    private String field;
+    private Integer salary;
+    private String country;
+    private String city;
+
+    public DreamJob() {
+    }
+
+    public DreamJob(String name, String field, Integer salary, String country, String city) {
+        this.name = name;
+        this.field = field;
+        this.salary = salary;
+        this.country = country;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+}
--- src/main/org.mykola.zakharov.startwith.hibernate/java/Main.java
+++ src/main/org.mykola.zakharov.startwith.hibernate/java/Main.java
@@ -4,16 +4,22 @@ import javax.persistence.EntityManager;
 
 public class Main {
     public static void main(String[] args) {
-
         SessionFactory sessionFactory =
                 HibernateFactory.getSessionFactory();
+
         User u = new User();
         u.setFirstName("f1");
         u.setLastName("l1");
         u.setAge(20);
+
         EntityManager em = sessionFactory.createEntityManager();
         em.getTransaction().begin();
         em.persist(u);
+        em.getTransaction().commit();
+
+        DreamJob dreamJob = new DreamJob("programmer", "finance", 25000, "Germany", "Munich");
+        em.getTransaction().begin();
+        em.persist(dreamJob);
         em.getTransaction().commit();
     }
 }
